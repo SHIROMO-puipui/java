17f3f6f50c1643e8dd2ecb6c422161854a1ff602
shiromo <a0981067552a@gmail.com>
2021-02-21 16:34:32 +0800

    java Pr

---
+++ 1-1/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       double radius;
+       double area;
+       double pi=3.14159;
+       radius=10;
+       area=radius*radius*pi;
+       System.out.println("Circle area"+area);
+    }
+}
